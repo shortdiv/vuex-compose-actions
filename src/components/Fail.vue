a3ae94d8460fe3493064e7761ab260ef6cc81297
Divya Tagtachian <hello@shortdiv.com>
2020-06-08 21:53:11 -0500

    add fail component

---
+++ src/components/Fail.vue
@@ -0,0 +1,65 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 243.45 234.24">
+    <title>Asset 10</title>
+    <g id="Layer_2" data-name="Layer 2">
+      <g id="Layer_1-2" data-name="Layer 1">
+        <polygon
+          class="cls-1"
+          points="41.73 73.18 120.73 43.18 201.73 73.18 120.73 107.18 41.73 73.18"
+        ></polygon>
+        <polygon
+          class="cls-2"
+          points="40.73 73.18 3.73 34.18 85.73 3.18 120.73 43.18 40.73 73.18"
+        ></polygon>
+        <polygon
+          class="cls-2"
+          points="157.73 1.18 240.73 32.18 203.06 72.68 120.73 43.18 157.73 1.18"
+        ></polygon>
+        <polyline
+          class="cls-3"
+          points="38.59 109.02 39.73 203.71 123.73 233.18 206.73 204.63 206.66 107.18"
+        ></polyline>
+        <line
+          class="cls-1"
+          x1="121.73"
+          y1="107.18"
+          x2="123.73"
+          y2="233.18"
+        ></line>
+        <polygon
+          class="cls-2"
+          points="120.73 107.18 157.73 148.18 241.73 117.18 202.73 73.18 120.73 107.18"
+        ></polygon>
+        <polygon
+          class="cls-2"
+          points="40.73 73.18 1.73 115.18 84.73 148.18 121.73 107.18 40.73 73.18"
+        ></polygon>
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "Fail"
+};
+</script>
+
+<style lang="scss" scoped>
+.cls-1 {
+  fill: none;
+}
+.cls-1,
+.cls-2,
+.cls-3 {
+  stroke: gray;
+  stroke-miterlimit: 10;
+  stroke-width: 2px;
+}
+.cls-2 {
+  fill: #ccc;
+}
+.cls-3 {
+  fill: #b3b3b3;
+}
+</style>
